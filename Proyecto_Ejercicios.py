a4b6ed613a249dfdbc5a348ddb08a8616286e221
Hector-Amaya <111697789+Hector-Amaya@users.noreply.github.com>
2024-04-26 17:58:24 -0600

    Create Proyecto_Ejercicios.py

---
+++ Proyecto_Ejercicios.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('Hola Mundo')
+st.header('Este es un encabezado')
+st.subheader('Este es un subencabezado')
+
+st.markdown(""" ### Esto es un texto en markdown""")
+st.markdown('### Esto es un texto en markdown')
+st.markdown("### Esto es un texto en markdown")
+
+st.markdown(""" - Esto es un texto en markdown, listado""")
+st.text('Este es un texto')
+st.write('Hola Mundo')
+
+st.markdown(""" [Esto es un texto en markdown, link] (https://www.google.com)""")
+st.markdown(" [Esto es un texto en markdown, link2] (https://www.ucol.mx/)")
