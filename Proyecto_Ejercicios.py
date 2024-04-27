d542768fcbf702aa9e6ff3e8f5aaec26047cd9f2
Hector-Amaya <111697789+Hector-Amaya@users.noreply.github.com>
2024-04-26 18:33:10 -0600

    Update Proyecto_Ejercicios.py

--- Proyecto_Ejercicios.py
+++ Proyecto_Ejercicios.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
-from sklearn.ensemble import IsolationForest
+# from sklearn.ensemble import IsolationForest
 import statistics as sti
 
 
