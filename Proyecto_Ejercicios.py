5ff878d08315e58c1611574bbbaadacae7b780de
Hector-Amaya <111697789+Hector-Amaya@users.noreply.github.com>
2024-04-26 18:02:46 -0600

    Update Proyecto_Ejercicios.py

--- Proyecto_Ejercicios.py
+++ Proyecto_Ejercicios.py
@@ -4,7 +4,34 @@ st.title('Hola Mundo')
 st.header('Este es un encabezado')
 st.subheader('Este es un subencabezado')
 
-st.markdown(""" ### Esto es un texto en markdown""")
+st.markdown("""
+<center>
+
+# Universidad de Colima
+### Campus Coquimatlán
+### Facultad de Ingeniería Mecánica y Eléctrica
+
+<br>
+
+## **Proyecto de Ejercicios de la 2da parcial en Streamlit**
+
+<br>
+</center>
+
+#### **Materia:** Análisis de Series Temporales
+#### **Maestro:** Mata López Walter Alexander
+
+<br>
+
+#### **Alumno:** Amaya González Héctor Eduardo
+
+#### **Carrera:** Ingeniería en Computación Inteligente
+#### **Semestre y Grupo:** 6°B
+
+<br>
+
+#### **Fecha de Entrega:** 26/04/2024
+""")
 st.markdown('### Esto es un texto en markdown')
 st.markdown("### Esto es un texto en markdown")
 
