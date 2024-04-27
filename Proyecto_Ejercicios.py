ecfb1942f1ba04da989fab98d62127662c33d1ed
Hector-Amaya <111697789+Hector-Amaya@users.noreply.github.com>
2024-04-26 18:56:05 -0600

    Update Proyecto_Ejercicios.py

--- Proyecto_Ejercicios.py
+++ Proyecto_Ejercicios.py
@@ -34,6 +34,7 @@ st.subheader('Este es un subencabezado')
 st.markdown("<center> ### Proyecto parcial", unsafe_allow_html=True)
 st.markdown("<center><h2 class='centered'>Facultad de Ingeniería Mecánica y Eléctrica</h2>", unsafe_allow_html=True)
 st.markdown("<h3 class='centered'>Ingeniería en Computación Inteligente</h3>", unsafe_allow_html=True)
+st.markdown("<center><h3Esto es un texto en markdown</h3>", unsafe_allow_html=True)
 
 
 st.markdown('### Esto es un texto en markdown')
