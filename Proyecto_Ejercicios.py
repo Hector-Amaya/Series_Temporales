fb05f37356a25e3b156c516938e6da5fea2c7fa0
Hector-Amaya <111697789+Hector-Amaya@users.noreply.github.com>
2024-04-26 18:37:40 -0600

    Update Proyecto_Ejercicios.py

--- Proyecto_Ejercicios.py
+++ Proyecto_Ejercicios.py
@@ -5,11 +5,63 @@ import numpy as np
 # from sklearn.ensemble import IsolationForest
 import statistics as sti
 
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: #f0f2f6;
+        color: #000000;
+    }
+    .sidebar .sidebar-content {
+        background: #212529;
+        color: #ffffff;
+    }
+    .Widget>label {
+        color: #ffffff;
+    }
+    .st-ck {
+        color: #ffffff;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title('Hola Mundo')
 st.header('Este es un encabezado')
 st.subheader('Este es un subencabezado')
 
+st.markdown("""
+<center>
+
+# Universidad de Colima
+### Campus Coquimatlán
+### Facultad de Ingeniería Mecánica y Eléctrica
+
+<br>
+
+## **Estadíticas descriptivas**
+
+<br>
+</center>
+
+#### **Materia:** Análisis de Series Temporales
+#### **Maestro:** Mata López Walter Alexander
+
+<br>
+
+#### **Alumno:** Amaya González Héctor Eduardo
+
+
+
+#### **Carrera:** Ingeniería en Computación Inteligente
+#### **Semestre y Grupo:** 6°B
+
+
+<br>
+
+#### **Fecha de Entrega:** 14/04/2024
+""")
 
 st.markdown('### Esto es un texto en markdown')
 st.markdown("### Esto es un texto en markdown")
@@ -32,7 +84,7 @@ option = st.sidebar.selectbox(
     ('Inicio', 'Configuración', 'Ayuda')
 )
 
-opcion = st.slider('Seleccione un valor')
+opcion = st.sidebar.multiselect()
 
 # Mostrar el contenido principal en función de la opción seleccionada
 if option == 'Inicio':
