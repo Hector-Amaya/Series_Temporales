81b8985cff10af424e0e2a1e70d9074fb7bb804d
Hector-Amaya <111697789+Hector-Amaya@users.noreply.github.com>
2024-04-26 18:29:40 -0600

    Update Proyecto_Ejercicios.py

--- Proyecto_Ejercicios.py
+++ Proyecto_Ejercicios.py
@@ -1,26 +1,15 @@
-# import streamlit as st
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import IsolationForest
+import statistics as sti
+
 
 st.title('Hola Mundo')
 st.header('Este es un encabezado')
 st.subheader('Este es un subencabezado')
 
-st.markdown("""
-# &nbsp; # Universidad de Colima
-# &nbsp; # Campus Coquimatlán
-# &nbsp; # Facultad de Ingeniería Mecánica y Eléctrica
-
-## **Proyecto de Ejercicios de la 2da parcial en Streamlit**
-
-#### **Materia:** Análisis de Series Temporales
-#### **Maestro:** Mata López Walter Alexander
-
-#### **Alumno:** Amaya González Héctor Eduardo
-
-#### **Carrera:** Ingeniería en Computación Inteligente
-#### **Semestre y Grupo:** 6°B
-
-#### **Fecha de Entrega:** 26/04/2024
-""")
 
 st.markdown('### Esto es un texto en markdown')
 st.markdown("### Esto es un texto en markdown")
@@ -31,3 +20,29 @@ st.write('Hola Mundo')
 
 st.markdown(""" [Esto es un texto en markdown, link] (https://www.google.com)""")
 st.markdown(" [Esto es un texto en markdown, link2] (https://www.ucol.mx/)")
+
+
+
+# Esto crea una barra lateral en la aplicación
+st.sidebar.title('Opciones')
+
+# Agrega elementos a la barra lateral
+option = st.sidebar.selectbox(
+    'Selecciona una opción',
+    ('Inicio', 'Configuración', 'Ayuda')
+)
+
+opcion = st.slider('Seleccione un valor')
+
+# Mostrar el contenido principal en función de la opción seleccionada
+if option == 'Inicio':
+    st.title('Página de inicio')
+    st.write('¡Bienvenido a nuestra aplicación!')
+
+elif option == 'Configuración':
+    st.title('Configuración')
+    st.write('¡Aquí puedes configurar tus preferencias!')
+
+elif option == 'Ayuda':
+    st.title('Ayuda')
+    st.write('¡Aquí puedes encontrar ayuda y soporte!')
